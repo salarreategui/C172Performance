feb7357d4891bdf4a98d7fcb72f32b7a6f75f4a8
Santiago <Santiago@macsln>
2015-09-21 09:38:59 -0500

    Revert "Enroute checkpoints showrows issue fixed"
    This reverts commit d472544aebce42716b31a25cce4285ee1082138b.

--- www/C172.js
+++ www/C172.js
@@ -1770,22 +1770,18 @@ function computeCheckpoints(){
                 } 
             }
             
-            if (i==1){
-                showRow("rowCheckpoint"+i, true);
+
+            if (CheckDist!=0 && CheckGS!=0){
+                showRow("rowCheckpoint"+CheckLastRow, true);
             }
-            else if (i>1){
-                if (CheckDist!=0 && CheckGS!=0){
-                    showRow("rowCheckpoint"+CheckLastRow, true);
+            else {
+                if (i!=CheckLastRow){
+                    SetCheckUTCTime(ATA+i,false);
                 }
-                else {
-                    if (i!=CheckLastRow){
-                        SetCheckUTCTime(ATA+i,false);
-                    }
-                    if (i>CheckLastRow && i>1){
-                        showRow("rowCheckpoint"+i, false);
-                    }
+                if (i>CheckLastRow && i>1){
+                    showRow("rowCheckpoint"+i, false);
                 }
-            }
+            }            
         };
         
 	computeCheck();
